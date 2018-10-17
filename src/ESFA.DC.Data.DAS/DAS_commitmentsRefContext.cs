e42f5a87188b7aec30153dc65af76a2e2c0ec398
John Youren <john.youren@gmail.com>
2018-10-17 14:20:52 +0100

    Added Das Commitments model

---
+++ src/ESFA.DC.Data.DAS/DAS_commitmentsRefContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ESFA.DC.Data.DAS.Model
+{
+    public partial class DAS_commitmentsRefContext : DbContext
+    {
+        public DAS_commitmentsRefContext()
+        {
+        }
+
+        public DAS_commitmentsRefContext(DbContextOptions<DAS_commitmentsRefContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DasCommitments> DasCommitments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DasCommitments>(entity =>
+            {
+                entity.HasKey(e => new { e.CommitmentId, e.VersionId });
+
+                entity.HasIndex(e => e.Uln);
+
+                entity.Property(e => e.VersionId).IsUnicode(false);
+
+                entity.Property(e => e.AccountLegalEntityPublicHashedId).IsUnicode(false);
+
+                entity.Property(e => e.PaymentStatusDescription).IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ src/ESFA.DC.Data.DAS/DasCommitments.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ESFA.DC.Data.DAS.Model
+{
+    public partial class DasCommitments
+    {
+        public long CommitmentId { get; set; }
+        [StringLength(25)]
+        public string VersionId { get; set; }
+        public long Uln { get; set; }
+        public long Ukprn { get; set; }
+        public long AccountId { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime StartDate { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime EndDate { get; set; }
+        [Column(TypeName = "decimal(15, 2)")]
+        public decimal AgreedCost { get; set; }
+        public long? StandardCode { get; set; }
+        public int? ProgrammeType { get; set; }
+        public int? FrameworkCode { get; set; }
+        public int? PathwayCode { get; set; }
+        public int PaymentStatus { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string PaymentStatusDescription { get; set; }
+        public int Priority { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime EffectiveFromDate { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? EffectiveToDate { get; set; }
+        [StringLength(100)]
+        public string LegalEntityName { get; set; }
+        public long? TransferSendingEmployerAccountId { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? TransferApprovalDate { get; set; }
+        public DateTime? PausedOnDate { get; set; }
+        public DateTime? WithdrawnOnDate { get; set; }
+        [StringLength(6)]
+        public string AccountLegalEntityPublicHashedId { get; set; }
+    }
+}
